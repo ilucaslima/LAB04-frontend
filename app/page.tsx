13273cad2f7e2279ad46e1397936c297e9048776
Paulo Fernandes Sousa Neto <paulofernandescj@gmail.com>
2026-07-07 23:50:06 -0300

    feat: adiciona componente Header na página principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Header from "../components/Header"; // Importando o header
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Talkflow 🚀</h1>
+    <main>
+      {/* Adicionamos o Header aqui em cima */}
+      <Header />
+      
+      {/* O resto da sua página */}
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-4xl font-bold">Talkflow 🚀</h1>
+      </div>
     </main>
   );
 }
