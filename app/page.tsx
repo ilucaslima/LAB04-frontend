dab6c3d0a7335de2d1b46601a6a3c209158455da
Paulo Fernandes Sousa Neto <paulofernandescj@gmail.com>
2026-07-29 10:16:42 -0300

    feat: implementa cabecalho e kanban

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
-import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import KanbanBoard from "../components/KanbanBoard";
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-gray-50 overflow-hidden">
-      {/* Menu Lateral esquerdo */}
-      <Sidebar />
-
-      {/* Área Principal (Direita) */}
-      <div className="flex-1 flex flex-col min-w-0 overflow-y-auto">
-        <Header />
-        <main className="flex-1">
-          <KanbanBoard />
-        </main>
-      </div>
-    </div>
+    <main className="min-h-screen bg-gray-50 flex flex-col">
+      {/* Cabeçalho no topo */}
+      <Header />
+      
+      {/* Quadro Kanban ocupando a tela */}
+      <KanbanBoard />
+    </main>
   );
 }
